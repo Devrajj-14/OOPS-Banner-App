f8d93201b80f18834740e92bc97c10c2a4498319
Devrajj-14 <dg9770@srmist.edu.in>
2026-02-04 17:23:46 +0530

    [Devraj]:mod

--- OOPS-Banner-App.java
+++ OOPS-Banner-App.java
@@ -5,9 +5,10 @@
  * @author Devraj
  * @version 1.0
  */
-public class OOPSBannerApp {
-
+cat > main.java <<'EOF'
+class OOPSBannerApp {
     public static void main(String[] args) {
         System.out.println("OOPS");
     }
 }
+EOF
