9f378f4e44c1bbaeb714a4eba80cc449bb6ea8b9
Devrajj-14 <dg9770@srmist.edu.in>
2026-02-04 17:28:36 +0530

    [Devraj]:all class names are OOPSBannerAPP

--- OOPS-Banner-App.java
+++ OOPS-Banner-App.java
@@ -5,10 +5,10 @@
  * @author Devraj
  * @version 1.0
  */
-cat > main.java <<'EOF'
-class OOPSBannerApp {
+public class main {
+
     public static void main(String[] args) {
         System.out.println("OOPS");
     }
 }
-EOF
+
